4ce197d09ebfed8fc904f386820c81e72c5fce1f
olafk <olaf.koziara@gmail.com>
2020-11-24 14:37:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
+var WebSocketServer = require("ws").Server;
+var http = require("http");
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + "/"))
+app.use(express.static(__dirname + "/"));
 
-var server = http.createServer(app)
-server.listen(port)
+var server = http.createServer(app);
+server.listen(port);
 
-console.log("http server listening on %d", port)
+console.log("http server listening on %d", port);
 
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+var wss = new WebSocketServer({ server: server });
+console.log("websocket server created");
 
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+wss.getUniqueID = function () {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + "-" + s4();
+};
+wss.on("connection", (ws) => {
+  if (!ws.id) {
+    ws.id = wss.getUniqueID();
+  }
+  wss.clients.forEach((client) => {
+    console.log(client.id);
+    client.send(JSON.stringify({ uid: client.id }));
+  });
 
-  console.log("websocket connection open")
+  ws.on("message", (data) => {
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN && client !== ws) {
+        const uid = JSON.parse(data).uid;
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+        if (uid === client.id) {
+          client.send(data);
+        }
+      }
+    });
+  });
+});
