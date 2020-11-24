eded6f2e5c6b3106c92eb29c8129edc6c2f3f694
olafk <olaf.koziara@gmail.com>
2020-11-24 14:47:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-var WebSocketServer = require("ws").Server;
-var http = require("http");
-var express = require("express");
-var app = express();
-var port = process.env.PORT || 5000;
+const WebSocketServer = require("ws").Server;
+const http = require("http");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + "/"));
 
-var server = http.createServer(app);
+const server = http.createServer(app);
 server.listen(port);
 
 console.log("http server listening on %d", port);
 
-var wss = new WebSocketServer({ server: server });
+const wss = new WebSocketServer({ server: server });
 console.log("websocket server created");
 
 wss.on("connection", function (ws) {
-  ws.id = 1;
   wss.clients.forEach((client) => {
     console.log(client.id);
     client.send(JSON.stringify({ uid: client.id }));
